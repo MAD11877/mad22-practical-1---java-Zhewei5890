b6b90db41c9b064bbef668f0082c38e9c8958ee4
Zhewei5890 <92966900+Zhewei5890@users.noreply.github.com>
2022-04-20 17:49:09 +0800

    Update Question2.java
    Answering Question 2

--- Question2.java
+++ Question2.java
@@ -11,6 +11,16 @@ public class Question2
      */
      
     Scanner in = new Scanner(System.in);
+    // System.out.print("Enter your height (in m): "); 
     double height = in.nextDouble();
+
+    //System.out.print("Enter your weight (in kg): ");
+    double weight = in.nextDouble();
+
+    double bmi = weight / (height * height);
+
+    //System.out.println("Your BMI is " + bmi);
+
+    
   }
 }
